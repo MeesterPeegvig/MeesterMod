be05cee0319572663867f19c4c405dda8049cd47
MeesterPeegvig <arumallavihal@gmail.com>
2021-12-02 22:02:21 -0800

    still getting startedx3

--- src/main/java/usa/co/MeesterPeegvig/meestermod/proxy/ClientProxy.java
+++ src/main/java/usa/co/MeesterPeegvig/meestermod/proxy/ClientProxy.java
@@ -4,15 +4,16 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class CommonProxy {
-    public void preInit(FMLPreInitializationEvent event){
-
-    }
-    public void init(FMLInitializationEvent event){
-
+public class ClientProxy extends CommonProxy{
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
     }
 
-    public void  postInit(FMLPostInitializationEvent event){
+    @Override
+    public void init(FMLInitializationEvent event) {
+    }
 
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
     }
 }
