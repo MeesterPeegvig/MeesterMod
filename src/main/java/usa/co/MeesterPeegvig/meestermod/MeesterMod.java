e617744b41845fe4ce4f2edab586986e9892e15a
MeesterPeegvig <arumallavihal@gmail.com>
2021-12-02 21:58:26 -0800

    still getting startedx2

--- src/main/java/usa/co/MeesterPeegvig/meestermod/MeesterMod.java
+++ src/main/java/usa/co/MeesterPeegvig/meestermod/MeesterMod.java
@@ -4,18 +4,26 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = MeesterMod.MODID, version = MeesterMod.VERSION)
-public class MeesterMod
-{
+@Mod(modid = MeesterMod.MODID, version = MeesterMod.VERSION, name = MeesterMod.NAME)
+public class MeesterMod {
     public static final String MODID = "meestermod";
     public static final String VERSION = "1.0";
     public static final String NAME = "MeesterMod";
-    
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
     }
 }
---
+++ src/main/java/usa/co/MeesterPeegvig/meestermod/proxy/CommonProxy.java
@@ -0,0 +1,4 @@
+package usa.co.MeesterPeegvig.meestermod.proxy;
+
+public class CommonProxy {
+}
